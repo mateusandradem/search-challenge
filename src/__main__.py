7693fd1281ecdcd9bab133a2296032c44cc8bcb6
Mateus Andrade <mateusandrade080@gmail.com>
2024-09-17 14:38:03 -0300

    Remove argv error handling

--- src/__main__.py
+++ src/__main__.py
@@ -1,15 +1,9 @@
-import sys
-
 import typer
 
 from words import search_words
 
 
 def main(words: list[str]):
-    if len(words) < 2:
-        print("Fonrneça ao menos 1 palavra")
-        sys.exit(1)
-
     matches = search_words(words)
 
     print(f'Foram encontradas {len(matches)} ocorrências pelo termo "{" ".join(words)}".')
